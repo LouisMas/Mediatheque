4d2f34b7d09d9e92e5b5bfdc4d37889993e7e366
Masurel Louis <mangaman77+>
2022-06-23 14:59:27 +0200

    Création des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonsComponent } from './commons/commons.component';
+import { NavbarComponent } from './commons/navbar/navbar.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductCardComponent } from './pages/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonsComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    PagesComponent,
+    HomePageComponent,
+    ProductsPageComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
