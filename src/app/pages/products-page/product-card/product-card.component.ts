497a341dffcc700db6730e7e622ff4484fe6ec5b
LouisMas <louis.masurel@sfr.fr>
2022-06-26 17:16:25 +0200

    Mis en place des composants principaux

--- src/app/pages/products-page/product-card/product-card.component.ts
+++ src/app/pages/products-page/product-card/product-card.component.ts
@@ -8,7 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductCardComponent implements OnInit {
 
   @Input()
-  product : any[] = [];
+  produit: any;
+  
+  DEFAULT_IMG : string = "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Funicorn-with-colorful-hear-on-rainbow-background-147517458.jpg&sp=1656053747Tfbe1d37e0a76991b95180470a251f265dab045609b264686d6af1dda3b95ab48"
   constructor() { }
 
   ngOnInit(): void {
